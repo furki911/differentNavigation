537dee083ac9c6f844cd8e0c44d766524d566ed7
Furqan Masood <bhatfurqan@gmail.com>
2024-05-21 00:27:29 +0530

    Bottom Tab Navigations Added

--- App.js
+++ App.js
@@ -1,49 +1,50 @@
 import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+// import { createTabNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StatusBar } from "expo-status-bar";
 import { Ionicons } from "@expo/vector-icons";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import UserScreen from "./screens/UserScreen";
 
-const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="light" />
-      <Drawer.Navigator
+      <BottomTab.Navigator
         initialRouteName="User"
         screenOptions={{
           headerStyle: {
             backgroundColor: "#3c0a6b",
           },
           headerTintColor: "white",
-          drawerActiveBackgroundColor: "#f0e1ff",
-          drawerActiveTintColor: "#3c0a6b",
+          // tabBarActiveBackgroundColor: "#f0e1ff",
+          tabBarActiveTintColor: "#3c0a6b",
           // drawerStyle: { backgroundColor: "#f0f0f0" },
         }}
       >
-        <Drawer.Screen
+        <BottomTab.Screen
           name="Welcome"
           component={WelcomeScreen}
           options={{
-            drawerLabel: "Home",
-            drawerIcon: ({ color, size }) => (
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, size }) => (
               <Ionicons name="home" color={color} size={size} />
             ),
           }}
         />
-        <Drawer.Screen
+        <BottomTab.Screen
           name="User"
           component={UserScreen}
           options={{
-            drawerIcon: ({ color, size }) => (
+            tabBarIcon: ({ color, size }) => (
               <Ionicons name="person" color={color} size={size} />
             ),
           }}
         />
-      </Drawer.Navigator>
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }
